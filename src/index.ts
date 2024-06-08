b369350e48b8dfbf3203bc2c12145e555f795c72
Hari Acharya <hari@blacktech.com.np>
2024-06-08 17:24:21 +0545

    feat: expose exceptions to user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { setExceptionMessage } from "./utils/configureMessage.util";
 import { setHandler } from "./utils/setHandler.util";
-import { exceptions } from "./exceptions";
+import CustomHttpException from "./exceptions/http.exception";
+import BadRequestException from './exceptions/badrequest.exception'
+import UnauthorizedException from './exceptions/badrequest.exception'
+import PaymentRequiredException from "./exceptions/paymentRequired.exception";
+import NotFoundException from "./exceptions/notFound.exception";
+import ForbiddenException from './exceptions/forbidden.exception'
+import MethodNotAllowedException from "./exceptions/methodNotAllowed.exception";
+import ConflictException from './exceptions/conflict.exception'
+import InternalServerErrorException from "./exceptions/internalServerError.exception";
+import TeaPotException from "./exceptions/teapot.exception";
 
 export {
   setExceptionMessage,
   setHandler,
-  exceptions
+  CustomHttpException,
+  BadRequestException,
+  UnauthorizedException,
+  PaymentRequiredException,
+  ForbiddenException,
+  NotFoundException,
+  MethodNotAllowedException,
+  ConflictException,
+  TeaPotException,
+  InternalServerErrorException
 }

03e3b4c44e10ea0d748beafbc4d00a84a13a0b5e
Hari Acharya <hari@blacktech.com.np>
2024-06-08 17:03:02 +0545

    chore: export exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { setExceptionMessage } from "./utils/configureMessage.util";
 import { setHandler } from "./utils/setHandler.util";
+import { exceptions } from "./exceptions";
 
 export {
   setExceptionMessage,
   setHandler,
+  exceptions
 }
